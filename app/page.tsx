2e73b51bbd2a63aa01675e9e3f444ff70200cd53
Alvaro Peña <me@alvropena.com>
2025-09-03 22:43:40 -0700

    Update intern status in Home component text

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 		<div className="max-w-sm flex flex-col gap-4">
 			<h1 className="text-2xl">Alvaro Peña</h1>
 			<p>
-				I&apos;m a self-taught Software Engineer. Currently, I&apos;m interning
+				I&apos;m a Software Engineer. Currently, I&apos;m interning
 				at{" "}
 				<Link
 					href="https://www.linkedin.com/company/favapp/"
